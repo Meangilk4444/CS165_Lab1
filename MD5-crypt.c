8fa4fc14e9eef3ea7f8c3e768ce8221eab936334
Meangilk4444 <priscilahuante123@gmail.com>
2021-01-08 15:10:22 -0800

    created md5 file

---
+++ MD5-crypt.c
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namesapce std;
+
+//1. initialization - gnerate a simple md5 hash based on the password and salt
+//2.Loop 1000 times, calculating a new MD5 hash based on the previous hash concatenated with alternatingly the password and the salt
+//3. finalization- use a special base64 encoding on the final hash to create the password hash string
+
+
